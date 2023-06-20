78aa551edb705217bcd2d07be68eaf75ece8f879
Anderson Jose Simplicio <anderson25eu@gmail.com>
2023-06-20 08:09:46 -0300

    Atividade 05 concluida

--- server-05/setup/db.js
+++ server-05/setup/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURL: 'mongodb+srv://anderson25eu:tQPzpVS3IbtuMSya@aula.4akpunl.mongodb.net/exemplo_db?retryWrites=true&w=majority',
+    mongoURL: 'mongodb+srv://anderson25eu:senha_secreta@aula.4akpunl.mongodb.net/exemplo_db?retryWrites=true&w=majority',
 };
